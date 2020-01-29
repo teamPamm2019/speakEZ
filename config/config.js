f1b13b860ef85306faf80a58beafbf42dd306d09
Aimee Bojorquez <abojo079@me.com>
2020-01-28 23:50:20 -0500

    server.js

--- config/config.js
+++ config/config.js
@@ -2,8 +2,8 @@
 const Sequelize = require("sequelize");
 
 // Creates mySQL connection using Sequelize, the empty string in the third argument spot is our password.
-const sequelize = new Sequelize("VirtualBar", "root", "", {
-  host: "localhost",
+const sequelize = new Sequelize("VirtualBar", "r17ie0jilw20yc4h", "ne89sjx1vz2636fe", {
+  host: "i5x1cqhq5xbqtv00.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
   port: 3306,
   dialect: "mysql",
   pool: {
